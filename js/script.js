ee19f8f0501e89a726849bdaa383e9fa5ca4f7fe
ELSON-341 <elson.sn314@gmail.com>
2024-03-27 15:16:23 -0300

    a upgrate

--- js/script.js
+++ js/script.js
@@ -5,8 +5,9 @@ const todoInput = document.querySelector('#todo-input')
 const todoList = document.querySelector('#todo-list')
 const editForm = document.querySelector('#edit-form')
 const editInput = document.querySelector('#edit-input')
-const cencelEditBtn = document.querySelector('#cancel-edit-btn')
+const cancelEditBtn = document.querySelector('#cancel-edit-btn')
 
+let oldInputValue
 
 // Funções 
 const saveTodo = (text) => {
@@ -38,6 +39,24 @@ const saveTodo = (text) => {
     todoInput.focus()
 }
 
+function toggleForms() {
+    editForm.classList.toggle('hide')
+    todoForm.classList.toggle('hide')
+    todoList.classList.toggle('hide')
+}
+
+function updateTodo(text) {
+    const todos = document.querySelectorAll('.todo')
+
+    todos.forEach((todo) => {
+        let todoTitle = todo.querySelector('h3') 
+
+        if(todoTitle.innerTextm === oldInputValue) {
+            todoTitle.innerText = text
+        }
+    })
+}
+
 // Eventos
 todoForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -49,18 +68,45 @@ todoForm.addEventListener('submit', (e) => {
 })
 document.addEventListener('click', (e) => {
     const targetEl = e.target
-    const parenEl = e.target.closest('div')
+    const parentEl = e.target.closest('div')
+    let todoTitle
+
+    if(parentEl && parentEl.querySelector('h3')) {
+        todoTitle = parentEl.querySelector('h3').innerText
+    }
     
     if(targetEl.classList.contains('finish-todo')){
         console.log('Clicou para finalizar')
-        parenEl.classList.toggle('done')
+        parentEl.classList.toggle('done')
     }
 
     if(targetEl.classList.contains('remove-todo')) {
-        parenEl.remove('done')
+        parentEl.remove('done')
     }
 
     if(targetEl.classList.contains('edit-todo')) {
-        console.log('test')
+
+        editInput.value = todoTitle
+        oldInputValue = todoTitle
+        toggleForms()
     }
 })
+
+
+cancelEditBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    
+    toggleForms()
+})
+
+editForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const editInputValue = editInput.value
+    
+    if(editInputValue) {
+        updateTodo(editInputValue)
+    }
+
+    toggleForms()
+}) 
